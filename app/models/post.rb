b5df7675f86064451703204fda2fbd43104b63db
tnkjapan <yuto.yasunaga@gmail.com>
2014-01-04 15:25:43 +0700

    Changed background

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ActiveRecord::Base
-  validates :text, presence: true, length: {minimum: 5}
+  validates :text, 
+    presence: true, length: {minimum: 5, too_short: '%{count}の長さは違います、一番短いのは５です。'}
 end
